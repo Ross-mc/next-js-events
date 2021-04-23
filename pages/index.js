bc0a6e7b54ab78746bcb4bad73be94d7d7ed6676
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-23 10:06:01 +0100

    converted all funcs to async, fetching data from firebase

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import EventList from "../components/events/EventList";
 import { getFeaturedEvents } from "../dummy-data"
 
 const Home = () => {
-  const featuredEvents = getFeaturedEvents();
+  const [featuredEvents, setFeaturedEvents] = useState([]);
+
+  getFeaturedEvents().then(res => setFeaturedEvents(res))
+  
+  // const featuredEvents = getFeaturedEvents();
 
 
 
   return (
     <div>
       <h1>Featured Events</h1>
-      <EventList items={featuredEvents}/>
+      {featuredEvents.length > 0 ? <EventList items={featuredEvents}/> : <h1>Loading...</h1>}
+      
     </div>
   )
 }
