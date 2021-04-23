982e4e50050280e99019a22ffab72862992dc734
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-23 11:44:27 +0100

    using get static props

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,37 @@ import { useState } from "react";
 import EventList from "../components/events/EventList";
 import { getFeaturedEvents } from "../API"
 
-const Home = () => {
-  const [featuredEvents, setFeaturedEvents] = useState([]);
+const Home = (props) => {
 
-  getFeaturedEvents().then(res => setFeaturedEvents(res))
-  
-  // const featuredEvents = getFeaturedEvents();
+  //*******************************************************
+  // the commented out code was my version for getting it on the client end which worked fine. But
+  // the tutorial was about using get static props which is what i have done below
+
+  // const [featuredEvents, setFeaturedEvents] = useState([]);
+
+  // getFeaturedEvents().then(res => setFeaturedEvents(res))
+
 
 
 
   return (
     <div>
       <h1>Featured Events</h1>
-      {featuredEvents.length > 0 ? <EventList items={featuredEvents}/> : <h1>Loading...</h1>}
+      {props.events.length > 0 ? <EventList items={props.events}/> : <h1>Loading...</h1>}
       
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents
+    }
+  }
+
 }
 
 export default Home
