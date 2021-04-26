af0e3c5508648d0923f3b619d40b1b0a5b59c4f3
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-26 10:56:24 +0100

    added default title and description which can be overwritten

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,16 @@ import Head from 'next/head'
 import Layout from '../components/layout/Layout'
 import '../styles/globals.css'
 
+//here we define a global head tag. This is overwritten if we specifically define the properties
+//in specific pages. But here it ensures we always have a defaul title, description etc.
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+        <title>NextJS Events</title>
+        <meta name="description" content="An App for finding programming events"/>
       </Head>
       <Component {...pageProps} />
     </Layout>
