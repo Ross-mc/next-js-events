084de2c12866c483f0d7ad40eb15d4c10a85bbe2
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-28 09:47:03 +0100

    wrap application in context

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,32 @@
-import Head from 'next/head'
-import Layout from '../components/layout/Layout'
-import Notification from '../components/notification/notification'
-import '../styles/globals.css'
+import Head from "next/head";
+import Layout from "../components/layout/Layout";
+import Notification from "../components/notification/notification";
+import { NotificationContextProvider } from "../store/notification-context";
+import "../styles/globals.css";
 
 //here we define a global head tag. This is overwritten if we specifically define the properties
 //in specific pages. But here it ensures we always have a defaul title, description etc.
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-        <title>NextJS Events</title>
-        <meta name="description" content="An App for finding programming events"/>
-      </Head>
-      <Component {...pageProps} />
-      <Notification title="Test" message="this is a test" status="pending" />
-    </Layout>
-  )
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <title>NextJS Events</title>
+          <meta
+            name="description"
+            content="An App for finding programming events"
+          />
+        </Head>
+        <Component {...pageProps} />
+        <Notification title="Test" message="this is a test" status="pending" />
+      </Layout>
+    </NotificationContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
